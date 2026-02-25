2785c59e5fe7dfc11a2d7c8ef538dc82a7595d8b
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-25 14:19:19 -0300

    Adiciona comentários

--- Mês 02 - Fevereiro/24.02.2025/Exercícios de revisão/Ex1.java
+++ Mês 02 - Fevereiro/24.02.2025/Exercícios de revisão/Ex1.java
@@ -5,8 +5,10 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 
+		// Scanner para ler os dados do usuário
 		Scanner input = new Scanner(System.in);
 
+		// DecimalFormat para formatar o salário mensal com duas casas decimais
 		DecimalFormat df = new DecimalFormat("0.00");
 
 		System.out.println("Insira seu número de matrícula");
@@ -25,6 +27,7 @@ public class Ex1 {
 
 		double salarioMensal = horasMensais * salarioHora;
 
+		// Saída com DecimalFormat para mostrar o salário mensal com duas casas decimais
 		System.out.println("O funcionário " + nome + " de matrícula: " + numeroMatricula + " teve um salário de "
 				+ df.format(salarioMensal));
 
