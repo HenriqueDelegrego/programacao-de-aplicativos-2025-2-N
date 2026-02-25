ef4ed1c3658c2265bf00849aa4b62d0a3ceb019b
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-25 14:20:58 -0300

    Adiciona comentários

--- Mês 02 - Fevereiro/24.02.2025/Exercícios de revisão/Ex5.java
+++ Mês 02 - Fevereiro/24.02.2025/Exercícios de revisão/Ex5.java
@@ -4,8 +4,8 @@ public class Ex5 {
 
 	public static void main(String[] args) {
 
+		// Scanner para ler os dados do usuário
 		Scanner input = new Scanner(System.in);
-		// atualização
 
 		System.out.println("Insira o número de matrícula");
 		String matricula = input.next();
@@ -13,14 +13,21 @@ public class Ex5 {
 		System.out.println("Insira a altura do atleta");
 		double altura = input.nextDouble();
 
+		// Variáveis para armazenar o atleta mais alto, mais baixo, suas matrículas, a
+		// soma das alturas e a quantidade de atletas
+		// Inicializamos o atleta mais alto com o menor valor possível e o mais baixo
+		// com o maior valor possível para garantir que qualquer altura inserida será
+		// comparada corretamente
 		double atletaMaisAlto = Double.MIN_VALUE;
 		double atletaMaisBaixo = Double.MAX_VALUE;
+
 		String matriculaMaisAlto = "";
 		String matriculaMaisBaixo = "";
 
 		double somaAltura = 0;
 		int qtdAtletas = 0;
 
+		// Loop para ler os dados dos atletas até que o número de matrícula seja "0"
 		while (!matricula.equals("0")) {
 
 			if (altura > atletaMaisAlto) {
