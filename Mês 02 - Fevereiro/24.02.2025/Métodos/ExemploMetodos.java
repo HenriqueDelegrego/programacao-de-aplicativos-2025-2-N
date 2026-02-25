bb45fe9e918059b464ff39daf69bccbfbcb913e9
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-24 21:06:18 -0300

    Adiciona exemplo métodos

---
+++ Mês 02 - Fevereiro/24.02.2025/Métodos/ExemploMetodos.java
@@ -0,0 +1,13 @@
+
+public class ExemploMetodos {
+
+	public static void imprimirTexto() {
+		System.out.println("Hello, world!");
+		System.out.println("Eu estou usando métodos pela primeira vez");
+	}
+
+	public static void main(String[] args) {
+		imprimirTexto();
+	}
+
+}
