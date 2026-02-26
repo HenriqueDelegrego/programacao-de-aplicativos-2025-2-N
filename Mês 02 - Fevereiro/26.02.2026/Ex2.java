631249ec67a4fcda5bde15215d104cfd366cb361
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-26 20:50:19 -0300

    Adiciona comentários no ex2

--- Mês 02 - Fevereiro/26.02.2026/Ex2.java
+++ Mês 02 - Fevereiro/26.02.2026/Ex2.java
@@ -10,6 +10,8 @@ public class Ex2 {
 
 		String nome = input.next();
 
+		// Utilizando o método equalsIgnoreCase para comparar os nomes, ignorando
+		// maiúsculas e minúsculas
 		if (nome.equalsIgnoreCase("João")) {
 			joaoPedro();
 		} else if (nome.equalsIgnoreCase("Kevin")) {
@@ -18,12 +20,18 @@ public class Ex2 {
 
 	}
 
+	/**
+	 * Método para exibir as informações do João Pedro
+	 */
 	public static void joaoPedro() {
 		System.out.println("O nome dele é João");
 		System.out.println("Ele tem 20 anos");
 		System.out.println("Ele é estudante de desenvolvimento de sistemas");
 	}
 
+	/**
+	 * Método para exibir as informações do Kevin
+	 */
 	public static void kevin() {
 		System.out.println("O nome dele é Kevin");
 		System.out.println("Ele tem 19 anos");
