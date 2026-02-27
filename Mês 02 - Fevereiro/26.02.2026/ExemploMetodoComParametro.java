e4c91188e1225e70ed47cc31c46a34559e68d9cb
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-02-26 21:04:31 -0300

    Adiciona exemplo de métodos com parãmetros

---
+++ Mês 02 - Fevereiro/26.02.2026/ExemploMetodoComParametro.java
@@ -0,0 +1,15 @@
+
+public class ExemploMetodoComParametro {
+
+	public static void somar(int num1, int num2) {
+		int resultado = num1 + num2;
+		System.out.println(resultado);
+	}
+
+	public static void main(String[] args) {
+
+		somar(10, 8);
+
+	}
+
+}
